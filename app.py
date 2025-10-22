ad5aa35e7a858d8e2b868bd398047bafd166895c
the-v-wolf <valentinwolf@icloud.com>
2025-10-22 17:13:25 +0200

    comiited new faeutre_file

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+from Feature_01 import return_even 
+
+original_list[i for i in range(10)]
+
+even_list = return_even(original_list)
+
 
 st.write ('Hello Sofiya and Valentin')
+
+
+
